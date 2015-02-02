e3d69cc6bf4c98ce03b5ee884a91fd492e1c0d55
Graham Ollis <perl@wdlabs.com>
2015-02-02 06:35:48 -0500

    give up on the compiler / link options too :P

--- inc/My/ModuleBuild.pm
+++ inc/My/ModuleBuild.pm
@@ -3,19 +3,16 @@ package My::ModuleBuild;
 use strict;
 use warnings;
 use Config;
+use ExtUtils::CppGuess;
 use base qw( Module::Build::FFI );
 
 sub new
 {
   my($class, %args) = @_;
-  
-  $args{extra_compiler_flags} = '-fkeep-inline-functions';  # g++ only
-  $args{extra_linker_flags}   = '-lstdc++';                 # probably also compiler dependent
-  
-  my $self = $class->SUPER::new(%args);
-
-  $self->config( cc => 'gcc' ) if $Config{cc} =~ /clang/; # clang does not support -fkeep-inline-functions
-  
+  my $self = $class->SUPER::new(
+    ExtUtils::CppGuess->new->module_build_options,
+    %args,
+  );
   $self;
 }
 
